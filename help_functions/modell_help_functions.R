b6e354a3b787d964bde29fdf17ab1d1b1bffc755
David <david.pro131996@gmail.com>
2021-12-01 17:43:29 +0100

    deaths_cases_rdy

---
+++ help_functions/modell_help_functions.R
@@ -0,0 +1,34 @@
+## help function
+inference_glm_seg <- function(modell) {
+  slopes <- slope(modell) 
+  conf_int <- confint(modell)
+  list(modell = list(modell), slopes = list(slopes), conf_int = list(conf_int),
+       BIC = BIC(modell))
+}
+
+seg_function <- function(modell) {
+  segmented(modell, psi = NA, control = seg.control(fix.npsi = FALSE, n.boot = 0, tol = 1e-7,
+                                                    it.max = 100, K = 12, display = FALSE))}
+
+# probably is 6 better against overfitting
+selg_function <- function(modell) selgmented(modell, type = "bic", Kmax = 12, msg = TRUE)
+
+# with list "list_selg_gamma[[1]]$selection.psi" sequentiell selection is possible
+seq_bic_modell <- function(modell, modell_selg) {
+  # Are the BICs of the breakpoints higher than before
+  logical_seq <- modell_selg$selection.psi[-12] > modell_selg$selection.psi[-1]
+  logical_seq[is.na(logical_seq)] <- FALSE
+  # picks first break point that is the last one which is smaller than befor
+  n_breakpoints <- ifelse(all(logical_seq), 12, which(!logical_seq)[1] - 1)
+  segmented(modell, npsi = n_breakpoints, control = seg.control(fix.npsi = FALSE, n.boot = 0, tol = 1e-7,
+                                                                it.max = 100, K = 12, display = TRUE))
+}
+
+function_plot <- function(modell_1, modell_2, modell_3, title) {
+  plot_gamma <- ggplot(data, aes(modell_1$y, x = rep_date_divi)) +
+    geom_point() +
+    geom_line(aes(y = modell_1$fitted.values, color = "fitted")) +
+    geom_line(aes(y = modell_2$fitted.values, color = "fitted_bic")) +
+    geom_line(aes(y = modell_3$fitted.values, color = "fitted_seq_bic")) +
+    labs(title = title)
+}
--- impl_model.R
+++ impl_model.R
@@ -3,92 +3,122 @@ library(data.table)
 library(ggplot2)
 library(checkmate)
 source("help_functions/seven_day_inzidenz.R")
-source("help_functions/columns_aggr_age.R")
-setDT(data)
+source("examples_of_code/example_aggregating.R")
+source("help_functions/modell_help_functions.R")
+setDT(main_data)
 #
 #
-
-# assumption: Gamma Distribution ------------------------------------------
-
-# the hist shows: the data ist bimodal and linkssteil
-hist(seven_day_inz(data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25"]$total_cases))
-#
-# assumption: log-Gaussian ------------------------------------------------
-# Data not really normaly distributed, but more than before
-hist(log(seven_day_inz(data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25"]$total_cases)))
+set.seed(1352674267)
+# assumption: Gamma Distribution for Cases ------------------------------------------
 
 
-## help function
-inference_glm_seg <- function(modell) {
-  slopes <- slope(modell) 
-  conf_int <- confint(modell)
-  list(modell = list(modell), slopes = list(slopes), conf_int = list(conf_int),
-       BIC = BIC(modell))
-}
+hist(main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25"]$seven_day_inz)
 
-age_groups_N <- c(83240000, 11477737, 18921292, 27600978, 18153339, 5936434)
-names(age_groups_N) <- c("total_cases", colnames(data)[96:100])
+
+
+sdi_cases_colnames <- c("seven_day_inz", "seven_day_inz_A00_A14", "seven_day_inz_A15_A34",
+                        "seven_day_inz_A35_A59", "seven_day_inz_A60_A79", "seven_day_inz_A80")
 
 # filter data
-data <- copy(data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25"])
+data <- copy(main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25"])
 
 # Gamma because smoothed (through 7-day Inzidenz) cases, arent "Zähldaten" &
 # log_normalverteilung doesnt fit very well (see playground_breakpoints.R)
-list_gamma_modells <- lapply(paste("seven_day_inz(", names(age_groups_N), ",", age_groups_N ,") ~ rep_date_divi"), 
+# making a list of models for all age groups
+list_gamma_sdi_modells <- lapply(paste(sdi_cases_colnames, " ~ rep_date_divi"), 
                              function(formula) fit_gamma <- glm(data = data, formula, family = Gamma(link = "log")))
 
-seg_function <- function(modell) {
-  segmented(modell, psi = NA, control = seg.control(fix.npsi = FALSE, n.boot = 0, tol = 1e-7,
-                                                       it.max = 100, K = 12, display = FALSE))}
-list_seg_gamma <- lapply(list_gamma_modells, seg_function)
+# fit the segmented model on all models with no specified breakpoints
+list_seg_sdi_gamma <- lapply(list_gamma_modells, seg_function)
 
-# probably is 6 better against overfitting
-selg_function <- function(modell) selgmented(modell, type = "bic", Kmax = 12, msg = TRUE)
+# fit the selgmented models on all models with the BIC criteria
+list_selg_sdi_gamma <- lapply(list_gamma_modells, selg_function)
 
-list_selg_gamma <- lapply(list_gamma_modells, selg_function)
+# fit the segmented models on all models with the sequentiell BIC criteria from selgmented
+list_bic_seq_sdi <- Map(seq_bic_modell, list_gamma_modells, list_selg_gamma)
 
-# with list "list_selg_gamma[[1]]$selection.psi" sequentiell selection is possible
-seq_bic_modell <- function(modell, modell_selg) {
-  # Are the BICs of the breakpoints higher than before
-  logical_seq <- modell_selg$selection.psi[-12] > modell_selg$selection.psi[-1]
-  # picks first break point that is the last one which is smaller than befor
-  n_breakpoints <- which(!logical_seq)[1] - 1
-  segmented(modell, npsi = n_breakpoints, control = seg.control(fix.npsi = FALSE, n.boot = 0, tol = 1e-7,
-                                                                 it.max = 100, K = 12, display = TRUE))
-}
-
-list_bic_seq <- Map(seq_bic_modell, list_gamma_modells, list_selg_gamma)
-
-
-function_plot <- function(modell_1, modell_2, modell_3, title) {
-  plot_gamma <- ggplot(data, aes(modell_1$y, x = rep_date_divi)) +
-    geom_point() +
-    geom_line(aes(y = modell_1$fitted.values, color = "fitted")) +
-    geom_line(aes(y = modell_2$fitted.values, color = "fitted_bic")) +
-    geom_line(aes(y = modell_3$fitted.values, color = "fitted_seq_bic")) +
-    labs(title = title)
-}
-
-plots <- Map(function_plot, list_seg_gamma, list_selg_gamma, list_bic_seq, names(age_groups_N))
+# plot for all models
+plots_sdi <- Map(function_plot, list_seg_gamma, list_selg_gamma, list_bic_seq, sdi_cases_colnames)
 #
 #
 
-# to compare we use a lm  -------------------------------------------------
+# to compare we use a log lm  -------------------------------------------------
 
-fit_lm <- lm(data = data, log(seven_day_inz(x_cases) + .1 * 10^(-5)) ~ rep_date_divi)
+# hist(log(seven_day_inz(data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25"]$total_cases)))
+# 
+# fit_lm <- lm(data = data, log(seven_day_inz(x_cases) + .1 * 10^(-5)) ~ rep_date_divi)
+# 
+# # no set breakpoints
+# seg_lm <- segmented(fit_lm, psi = list(x = NA, z = .3), 
+#                     control = seg.control(fix.npsi = FALSE, n.boot = 0, tol = 1e-7, it.max = 50, K = 12, display = TRUE))
+# 
+# # probably is 6 better against overfitting
+# selg_lm <- selgmented(fit_lm, type = "bic", Kmax = 12)
+# 
+# 
+# ggplot(data = data, aes(x = rep_date_divi, y =  log(seven_day_inz(x_cases) + .1 * 10^(-5)))) +
+#   geom_point() +
+#   geom_line(aes(y = seg_lm$fitted.values, color = "fitted")) +
+#   geom_line(aes(y = selg_lm$fitted.values, color = "fitted_selg"))
+# 
+# modells <- rbindlist(lapply(list(seg_gamma, selg_gamma, seg_lm, selg_lm), inference_glm_seg))
+# list(modells = modells, plot_gamma = plot_gamma, plot_lm = plot_lm)
 
-# no set breakpoints
-seg_lm <- segmented(fit_lm, psi = list(x = NA, z = .3), 
-                    control = seg.control(fix.npsi = FALSE, n.boot = 0, tol = 1e-7, it.max = 50, K = 12, display = TRUE))
 
-# probably is 6 better against overfitting
-selg_lm <- selgmented(fit_lm, type = "bic", Kmax = 12)
+# Gamma assumption: Death Cases -------------------------------------------------------------
+
+hist(main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25"]$seven_day_death_inz_A80)
+
+sdi_deaths_colnames <- c("seven_day_death_inz", "seven_day_death_inz_A00_A14", "seven_day_death_inz_A15_A34",
+                        "seven_day_death_inz_A35_A59", "seven_day_death_inz_A60_A79", "seven_day_death_inz_A80")
+
+# filter data
+data <- copy(main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25"])
+
+# Gamma because smoothed (through 7-day Inzidenz) cases, arent "Zähldaten" &
+# log_normalverteilung doesnt fit very well (see playground_breakpoints.R)
+# making a list of models for all age groups
+list_gamma_death_modells <- lapply(paste(sdi_deaths_colnames, " + 0.0001 ~ rep_date_divi"), 
+                             function(formula) fit_gamma <- glm(data = data, formula, family = Gamma(link = "log")))
+
+# fit the segmented model on all models with no specified breakpoints
+list_seg_death_gamma <- lapply(list_gamma_death_modells, seg_function)
+
+# fit the selgmented models on all models with the BIC criteria
+list_selg_death_gamma <- lapply(list_gamma_death_modells, selg_function)
+
+# fit the segmented models on all models with the sequentiell BIC criteria from selgmented
+list_bic_seq_death <- Map(seq_bic_modell, list_gamma_death_modells, list_selg_gamma)
+
+# plot for all models
+plots_death <- Map(function_plot, list_seg_death_gamma, list_selg_death_gamma, list_bic_seq_death, sdi_deaths_colnames)
+
+# Gamma assumption: Death Cases -------------------------------------------------------------
+
+hist(main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25"]$seven_day_death_inz_A80)
+
+sdi_deaths_colnames <- c("seven_day_death_inz", "seven_day_death_inz_A00_A14", "seven_day_death_inz_A15_A34",
+                         "seven_day_death_inz_A35_A59", "seven_day_death_inz_A60_A79", "seven_day_death_inz_A80")
+
+# filter data
+data <- copy(main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25"])
+
+# Gamma because smoothed (through 7-day Inzidenz) cases, arent "Zähldaten" &
+# log_normalverteilung doesnt fit very well (see playground_breakpoints.R)
+# making a list of models for all age groups
+list_gamma_death_modells <- lapply(paste(sdi_deaths_colnames, " + 0.0001 ~ rep_date_divi"), 
+                                   function(formula) fit_gamma <- glm(data = data, formula, family = Gamma(link = "log")))
+
+# fit the segmented model on all models with no specified breakpoints
+list_seg_death_gamma <- lapply(list_gamma_death_modells, seg_function)
+
+# fit the selgmented models on all models with the BIC criteria
+list_selg_death_gamma <- lapply(list_gamma_death_modells, selg_function)
+
+# fit the segmented models on all models with the sequentiell BIC criteria from selgmented
+list_bic_seq_death <- Map(seq_bic_modell, list_gamma_death_modells, list_selg_gamma)
+
+# plot for all models
+plots_death <- Map(function_plot, list_seg_death_gamma, list_selg_death_gamma, list_bic_seq_death, sdi_deaths_colnames)
 
 
-ggplot(data = data, aes(x = rep_date_divi, y =  log(seven_day_inz(x_cases) + .1 * 10^(-5)))) +
-  geom_point() +
-  geom_line(aes(y = seg_lm$fitted.values, color = "fitted")) +
-  geom_line(aes(y = selg_lm$fitted.values, color = "fitted_selg"))
-
-modells <- rbindlist(lapply(list(seg_gamma, selg_gamma, seg_lm, selg_lm), inference_glm_seg))
-list(modells = modells, plot_gamma = plot_gamma, plot_lm = plot_lm)
