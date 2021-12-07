c17a8dea98fd834bedcad351d70a98681aeef241
Prokosch <D.Prokosch@campus.lmu.de>
2021-12-07 10:18:35 +0100

    plots_model_rdy

--- examples_of_code/example_aggregating.R
+++ examples_of_code/example_aggregating.R
@@ -58,7 +58,7 @@ main_data[, A00_A14_death_cases := sum(.SD),
 
 main_data$seven_day_inz <- seven_day_inz(main_data$total_cases)
 main_data$seven_day_death_inz <- seven_day_inz(main_data$total_death_cases)
-main_data$seven_day_hosp_inz <- seven_day_inz(main_data$beds_occupied_divi)
+main_data$seven_day_hosp_inz <- seven_day_inz(main_data$cases_covid_divi)
 
 
 main_data$seven_day_inz_A00_A14 <- seven_day_inz(main_data$A00_A14_cases, 11477737)
