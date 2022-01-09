eb31c7835f76031fd2f25c80a2a9ffa32c8922c3
davidprokosch <d.prokosch@campus.lmu.de>
2022-01-09 13:06:43 +0100

    added_title

--- meeting_viz_file.R
+++ meeting_viz_file.R
@@ -4,33 +4,33 @@ source("Praktikum.R")
 ccf(main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-01-01",
               seven_day_inz],
     main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-01-01",
-              seven_day_death_inz])
+              seven_day_death_inz], main = "01.01. - Ende Vergleichszeitraum")
 # Im vgl ungeklaettet
 ccf(main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-01-01",
               total_cases],
     main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-01-01",
-              total_death_cases])
+              total_death_cases], main = "01.01. - Ende Vergleichszeitraum (ungeklättet)")
 
 # Vom 01.01.2020 bis zum Start unseres Beobachtungszeitraums
 ccf(main_data["2020-10-01" > rep_date_divi & rep_date_divi >= "2020-01-01",
               seven_day_inz],
     main_data["2020-10-01" > rep_date_divi & rep_date_divi >= "2020-01-01",
-              seven_day_death_inz])
+              seven_day_death_inz], main = "01.01. - Anfang Vergleichszeitraum")
 
 # Vergleichszeitraum
 ccf(main_data["2020-09-30" > rep_date_divi & rep_date_divi >= "2020-06-01",
               seven_day_inz],
     main_data["2020-09-30" > rep_date_divi & rep_date_divi >= "2020-06-01",
-              seven_day_death_inz])
+              seven_day_death_inz], main = "Vergleichszeitraum")
 
 # Vom 01.01.2020 bis zum Start des Vergleichszeitraums
 ccf(main_data["2020-06-01" > rep_date_divi & rep_date_divi >= "2020-01-01",
               seven_day_inz],
     main_data["2020-06-01" > rep_date_divi & rep_date_divi >= "2020-01-01",
-              seven_day_death_inz])
+              seven_day_death_inz], main = "01.01. - Anfang Vergleichszeitraum")
 
 # unseres Beobachtungszeitraums
 ccf(main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25",
               seven_day_inz],
     main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-09-25",
-              seven_day_death_inz])
+              seven_day_death_inz], main = "Beobachtungszeitraum")
