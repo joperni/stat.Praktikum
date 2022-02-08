8e7ab18b3e24ec04c63ff14f88d1fd6d01b09ffb
Prokosch <D.Prokosch@campus.lmu.de>
2022-02-08 18:02:01 +0100

    added_ratio_and_korinz_models

--- implementation_kor_inz_models.R
+++ implementation_kor_inz_models.R
@@ -1,21 +1,22 @@
 source("implementation_models.R")
 source("Praktikum.R")
 
-dt_kor_inz <- copy(data_okt_dez[, .(date = as.numeric(rep_date_divi), .SD),
+
+# data from "Praktikum.R"
+dt_kor_inz <- copy(data_okt_dez[, .(rep_date = as.numeric(rep_date_divi), .SD),
                                 .SDcols = colnames(data_okt_dez)[grepl("kor_inz", colnames(data_okt_dez))]][
                                   , .SD.kor_inz_A00_A14 := NULL])
-formulas_kor <- paste(colnames(dt_kor_inz)[-1], "+ 1 * exp(5) ~ date")
+# is dependent from the sequence of the columns in "Praktikum.R"
+setcolorder(dt_kor_inz, c(1, 6, 2:4))
+formulas_kor <- paste(colnames(dt_kor_inz)[-1], "+ 1 * exp(5) ~ rep_date")
 
 dt_kor_models <- data.table(formulas = formulas_kor)
 
-dt_kor_models[, base_model := lapply(formulas, glm, dt_kor_inz, family = Gamma(link = "log"))]
-
 dt_kor_models[, seg_fixed_bp := lapply(1:5, function(x) {
-  segmented(dt_kor_models[x, base_model][[1]], fixed.psi = list(dt_models[x, model_bic_seq[[1]]$psi[, 2]]))
+  # first fit the base model
+  segmented(glm(formulas_kor[1], dt_kor_inz, family = Gamma(link = "log")),
+            # psi from the fitted models for the not corrected inzidenz
+            psi = dt_models[x, model_bic_seq[[1]]$psi[, 2]],
+            # (it.max = 0) to avoid new bps
+            control = seg.control(it.max = 0))
   })]
-              
-
-segmented(dt_kor_models[1, base_model][[1]], fixed.psi = dt_models[1, model_bic_seq[[1]]$psi[, 2]])
-
-seg_debug <- segmented(glm(formulas_kor[1], dt_kor_inz, family = Gamma(link = "log")),
-                       fixed.psi = dt_models[2, model_bic_seq[[1]]$psi[, 2]], control = seg.control(it.max = 30))
---
+++ implementation_ratio_models.R
@@ -0,0 +1,48 @@
+library(segmented)
+library(ggplot2)
+library(checkmate)
+library(gridExtra)
+library(RColorBrewer)
+library(dplyr)
+library(cowplot)
+library(grid)
+source("examples_of_code/example_aggregating.R")
+source("help_functions/model_help_functions.R")
+setDT(main_data)
+
+#  Choose our "beobachtungszeitraum"
+data <- copy(main_data["2020-12-23" > rep_date_divi & rep_date_divi >= "2020-10-01"])
+
+set.seed(1352674267)
+
+age <- c("overall", "15_34", "35_59", "60_79", "80")
+
+sdi_cases_colnames <- c("seven_day_inz", "seven_day_inz_A15_A34",
+                        "seven_day_inz_A35_A59", "seven_day_inz_A60_A79", "seven_day_inz_A80")
+sdi_deaths_colnames <- c("seven_day_death_inz", "seven_day_death_inz_A15_A34",
+                         "seven_day_death_inz_A35_A59", "seven_day_death_inz_A60_A79", "seven_day_death_inz_A80")
+data[, paste("ratio", age, sep = "_") := 
+       data[, .SD, .SDcols = sdi_deaths_colnames] / data[, .SD, .SDcols = sdi_cases_colnames]]
+
+formulas_ratio <- paste(paste("ratio", age, sep = "_"), " ~ rep_date_divi")
+
+# change data type of the date, because otherwise predicting doesnt work
+data$rep_date_divi <- as.numeric(data$rep_date_divi)
+
+dt_models_ratio <- data.table(formulas = formulas_ratio)
+# add a lm that is needed for the segmented/selgmented function
+dt_models_ratio[, base_model := lapply(formulas, function(x) lm(data = data, x))]
+
+# using selg_function from "help_functions/model_help_functions.R" to create a bic for each number of breakpoints
+# error messages are no problem. They get prduced and catched by the segmented::selgmented function
+dt_models_ratio[, model_bic := lapply(base_model, selg_function)
+          # choosing the model with the sequential lowest BIC for the number of breakpoints
+][, model_bic_seq := Map(seq_bic_model, base_model, model_bic)
+  # making a confint matrix with the predicted y for the estimated breakpoints
+][, confint := lapply(model_bic_seq, function(model) {
+  conf_matrix <- confint(model)
+  # bind the conf_matrix with a prediction for the estimated breakpoints
+  new_matrix <- cbind(conf_matrix,
+                      y = predict(model, newdata = data.frame(rep_date_divi = conf_matrix[, 1]),
+                                  type = "response"))})
+]
